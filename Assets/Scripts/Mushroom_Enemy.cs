471f1bcc57218a987a37a8d3a08f199ab74897c7
Dylan Owens <dylan.owens06@ed.cna.nl.ca>
2023-04-12 14:01:37 -0230

    + Fixing player hit

--- Assets/Scripts/Mushroom_Enemy.cs
+++ Assets/Scripts/Mushroom_Enemy.cs
@@ -4,19 +4,15 @@ using UnityEngine;
 
 public class Mushroom_Enemy : MonoBehaviour
 {
+
     public float damage = 1;
 
 void OnTriggerEnter2D(Collider2D other) {
-    IDamageable damageable = other.GetComponent<IDamageable>();
+        IDamageable damageable = other.GetComponent<IDamageable>();
 
-    if (damageable != null) {
-        // Define knockback force
-        Vector2 knockbackForce = (other.transform.position - transform.position).normalized * 5f;
-        
-        // Call OnHit method with damage and knockback force
-        damageable.OnHit(damage, knockbackForce);
+        if (damageable != null) {
+            damageable.OnHit(damage);
+        }
     }
-}
-}
 
-
+}
