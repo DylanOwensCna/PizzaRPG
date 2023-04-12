8276b9eac7469a5381457abb676746131df65c41
Dylan Owens <dylan.owens06@ed.cna.nl.ca>
2023-04-12 20:47:59 -0230

    + Player Movement locks and game ends when health is<0

--- Assets/Character/DamageableCharacter.cs
+++ Assets/Character/DamageableCharacter.cs
@@ -8,62 +8,60 @@ public class DamageableCharacter : MonoBehaviour, IDamageable
     Animator animator;
     Rigidbody2D rb;
     Collider2D physicsCollider;
-    bool isAlive = true;
-public float Health {
-        set {
-            if(value < _health) {
-                animator.SetTrigger("hit");
-
-            _health = value;
-            
-            }
-            if(_health <=0 ){
-                // Defeated();
-                animator.SetBool("isAlive", false);
-                Targetable = false; 
-            }
-            
+    bool isAlive = true;public float Health {
+    set {
+        if(value < _health) {
+            animator.SetTrigger("hit");
         }
-        get {
-            return _health;
+        
+        _health = value;
+
+        if(_health <= 0) {
+            Defeated();
         }
     }
-
-    
-    public void Start(){
-        animator = GetComponent<Animator>();
-        animator.SetBool("isAlive", isAlive);
-        rb = GetComponent<Rigidbody2D>();
-
-        physicsCollider = GetComponent<Collider2D>();
+    get {
+        return _health;
     }
-    
-    public bool Targetable { get { return _targetable; }
+}
+
+public float _health = 3;
+public bool _targetable = true;
+
+public void Start() {
+    animator = GetComponent<Animator>();
+    animator.SetBool("isAlive", isAlive);
+    rb = GetComponent<Rigidbody2D>();
+    physicsCollider = GetComponent<Collider2D>();
+}
+
+public bool Targetable {
+    get {
+        return _targetable;
+    }
     set {
         _targetable = value;
-        
         physicsCollider.enabled = value;
-    } }
-
-    public float _health = 3;
-    public bool _targetable = true;
-
-    public void OnHit(float damage, Vector2 knockback)
-    {
-        Health -= damage;
-        // Apply the force to the slime
-        rb.AddForce(knockback);
     }
-
-    public void OnHit(float damage)
-    {
-
-        Health -= damage;
-    }
-
-    public void OnObjectDestroyed() {
-        Destroy(gameObject);
-    }
-
-
 }
+
+public void OnHit(float damage, Vector2 knockback) {
+    Health -= damage;
+    rb.AddForce(knockback);
+}
+
+public void OnHit(float damage) {
+    Health -= damage;
+}
+
+public void OnObjectDestroyed() {
+    Destroy(gameObject);
+}
+
+private void Defeated() {
+    Time.timeScale = 0; // Stop the game
+    // Show the "Game Over" screen
+   
+}
+}
+
